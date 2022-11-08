801cf1fde363e9b5e6511904abf7c591d24518f8
yeonjy <yeonjypaper@gmail.com>
2022-11-09 05:09:33 +0900

    Initial commit

---
+++ src/main/java/com/example/techeerteama1/TecheerTeamA1Application.java
@@ -0,0 +1,13 @@
+package com.example.techeerteama1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecheerTeamA1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TecheerTeamA1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/techeerteama1/TecheerTeamA1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.techeerteama1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecheerTeamA1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
